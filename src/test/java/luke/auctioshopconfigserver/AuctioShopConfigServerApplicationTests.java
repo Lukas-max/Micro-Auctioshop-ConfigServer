d3bb21fa0ac2ef0e89fae0d0f5ee5246b5dab3a7
Lukas-max <lukaszjan88@gmail.com>
2021-01-28 10:42:04 +0100

    Initial commit

---
+++ src/test/java/luke/auctioshopconfigserver/AuctioShopConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package luke.auctioshopconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctioShopConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
